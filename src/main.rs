ce93fc98e6f33676e20bbe5242872f43d7dd63b5
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-05-11 10:40:46 -0400

    next update (slight refactor)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-use lox;
+use lox::chunk;
+use lox::opcode::Operation;
+use lox::vm::VM;
 
 fn main() {
-    println!("Hello, world!");
+    let mut chunk = chunk::Chunk::new();
+    chunk.add_constant(52, 1);
+    chunk.add_constant(12, 2);
+    chunk.write(Operation::Return, 3);
+    chunk.add_constant(14, 4);
+    let mut vm = VM::new(chunk);
+    vm.interpret().expect("yup");
 }
